d53f6ca3b14bedd7f92abe7e8bd84d268a34bc18
Azimkhan <158446872+a-nurpeissov@users.noreply.github.com>
2025-04-16 15:55:35 +0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -9,14 +9,6 @@ class Category(models.Model):
         return self.name
 
 
-class PlaceManager(models.Manager):
-    def visited(self):
-        return self.filter(is_visited=True)
-
-    def unvisited(self):
-        return self.filter(is_visited=False)
-
-
 class Place(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
@@ -26,9 +18,6 @@ class Place(models.Model):
     image = models.ImageField(upload_to='place_images/', null=True, blank=True)
     is_visited = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-
-    objects = models.Manager()
-    visited_places = PlaceManager()
 
 
 class Review(models.Model):
@@ -40,12 +29,3 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.user.username} on {self.place.title}"
-
-
-class Photo(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='photos')
-    image = models.ImageField(upload_to='place_photos/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Photo for {self.place.title}"
